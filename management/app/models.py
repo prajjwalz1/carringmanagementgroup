75f24f08639029d7759a4984d65c6bde35d8cdc9
prajjwal <acharyaprajjwol152@gmail.com>
2023-08-26 17:04:57 +0545

    vercel deployment configuration

--- management/app/models.py
+++ management/app/models.py
@@ -285,3 +285,22 @@ class FormSubmission(models.Model):
     availabilities = models.ManyToManyField(Availability)
     def __str__(self):
         return self.firstname
+
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils import six
+class TokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp) +
+            six.text_type(user.is_active)
+        )
+account_activation_token = TokenGenerator()
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
